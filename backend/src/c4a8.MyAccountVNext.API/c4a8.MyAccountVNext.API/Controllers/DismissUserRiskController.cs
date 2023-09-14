907e5e5614b52ff3b2150cf036f037b0748cbb35
HeinzLm <13243189+HeinzLm@users.noreply.github.com>
2023-09-14 10:29:02 +0200

    api: add dummy claims challenge

--- backend/src/c4a8.MyAccountVNext.API/c4a8.MyAccountVNext.API/Controllers/DismissUserRiskController.cs
+++ backend/src/c4a8.MyAccountVNext.API/c4a8.MyAccountVNext.API/Controllers/DismissUserRiskController.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Identity.Web;
+using System.Globalization;
+using System.Net;
 using System.Security.Claims;
+using System.Text;
 
 namespace c4a8.MyAccountVNext.API.Controllers
 {
@@ -20,18 +23,25 @@ namespace c4a8.MyAccountVNext.API.Controllers
         [HttpPut]
         public IActionResult Get()
         {
-            string claimsChallenge = CheckForRequiredAuthContext(Request.Method);
-            if (!string.IsNullOrWhiteSpace(claimsChallenge))
-            {
-                _consentHandler.ChallengeUser(new string[] { "MyAccount.VNext.DismissUserRisk" }, claimsChallenge);
+            //string claimsChallenge = CheckForRequiredAuthContext(Request.Method);
+            //if (!string.IsNullOrWhiteSpace(claimsChallenge))
+            //{
+            //    _consentHandler.ChallengeUser(new string[] { "MyAccount.VNext.DismissUserRisk" }, claimsChallenge);
 
-                return new EmptyResult();
-            }
-
-            return new StatusCodeResult(StatusCodes.Status200OK);
+            //    return new EmptyResult();
+            //}
+            string missingAuthContextId = "c1";
+            var base64str = Convert.ToBase64String(Encoding.UTF8.GetBytes("{\"access_token\":{\"acrs\":{\"essential\":true,\"value\":\"" + missingAuthContextId + "\"}}}"));
+            var context = HttpContext;
+            context.Response.Headers.Append("WWW-Authenticate", $"Bearer realm=\"\", authorization_uri=\"https://login.microsoftonline.com/common/oauth2/authorize\", error=\"insufficient_claims\", claims=\"" + base64str + "\"");
+            context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+            string message = string.Format(CultureInfo.InvariantCulture, "The presented access tokens had insufficient claims. Please request for claims requested in the WWW-Authentication header and try again.");
+            context.Response.WriteAsync(message);
+            context.Response.CompleteAsync();
+            throw new UnauthorizedAccessException(message);
         }
 
-        public string CheckForRequiredAuthContext(string method)
+        private string CheckForRequiredAuthContext(string method)
         {
             string claimsChallenge = string.Empty;
 
