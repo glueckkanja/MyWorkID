a14a4f1df63ddad6b0e4b741d8da8dc0b723bfdf
Michael Mican <45571948+MichaMican@users.noreply.github.com>
2023-10-26 11:06:51 +0200

    add healthy check endpoint

---
+++ backend/src/c4a8.MyAccountVNext.API/c4a8.MyAccountVNext.API/Controllers/GeneralController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace c4a8.MyAccountVNext.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GeneralController : ControllerBase
+    {
+        [AllowAnonymous]
+        [HttpGet]
+        public ActionResult<string> LandingPage()
+        {
+            return Ok("Healthy");
+        }
+    }
+}
