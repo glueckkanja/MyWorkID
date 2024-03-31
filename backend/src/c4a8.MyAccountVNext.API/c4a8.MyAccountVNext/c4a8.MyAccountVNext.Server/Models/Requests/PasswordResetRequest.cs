f0b3c05f9644386ed3c93b2908456ee01bd1ac84
HeinzLm <13243189+HeinzLm@users.noreply.github.com>
2024-04-01 00:25:53 +0200

    backend - add password reset endpoint

---
+++ backend/src/c4a8.MyAccountVNext.API/c4a8.MyAccountVNext/c4a8.MyAccountVNext.Server/Models/Requests/PasswordResetRequest.cs
@@ -0,0 +1,10 @@
+﻿using c4a8.MyAccountVNext.Server.Models.Validation;
+
+namespace c4a8.MyAccountVNext.Server.Models.Requests
+{
+    public class PasswordResetRequest
+    {
+        [ValidatePassword]
+        public string NewPassword { get; set; }
+    }
+}
