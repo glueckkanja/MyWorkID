615c2d00e1be2df76739b322b47da702864bc98c
Michael Mican <45571948+MichaMican@users.noreply.github.com>
2024-04-15 14:23:45 +0200

    remove unused Authorization handlers

--- backend/src/c4a8.MyAccountVNext.API/c4a8.MyAccountVNext/c4a8.MyAccountVNext.Server/AuthorizationHandler/VerfiedIdCallbackAuthorizationRequirement.cs
+++
@@ -1,8 +0,0 @@
-﻿//using Microsoft.AspNetCore.Authorization;
-
-//namespace c4a8.MyAccountVNext.Server.AuthorizationHandler
-//{
-//    public class VerfiedIdCallbackAuthorizationRequirement : IAuthorizationRequirement
-//    {
-//    }
-//}
