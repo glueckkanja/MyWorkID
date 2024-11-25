348d533d80330a4c159720cd8c077f1444d0b16f
Stefan Heller <stefan.heller@glueckkanja-gab.com>
2024-11-25 14:10:32 +0100

    fix input password

--- backend/src/c4a8.MyAccountVNext.API/c4a8.MyAccountVNext/c4a8.myaccountvnext.client/src/components/function-plane/function-plane-components/password-reset.tsx
+++ backend/src/c4a8.MyAccountVNext.API/c4a8.MyAccountVNext/c4a8.myaccountvnext.client/src/components/function-plane/function-plane-components/password-reset.tsx
@@ -55,7 +55,62 @@ const svgIcon = (
     </g>
   </svg>
 );
-
+const eyeIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="20"
+    height="12"
+    viewBox="0 0 20 12"
+    fill="none"
+  >
+    <path
+      d="M7.46298 6.00005C7.46298 6.71409 7.74664 7.39889 8.25154 7.9038C8.75645 8.4087 9.44125 8.69236 10.1553 8.69236C10.8693 8.69236 11.5541 8.4087 12.059 7.9038C12.5639 7.39889 12.8476 6.71409 12.8476 6.00005C12.8476 5.286 12.5639 4.6012 12.059 4.0963C11.5541 3.59139 10.8693 3.30774 10.1553 3.30774C9.44125 3.30774 8.75645 3.59139 8.25154 4.0963C7.74664 4.6012 7.46298 5.286 7.46298 6.00005Z"
+      stroke="#7F7F7F"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M18.9091 5.74615C18.0091 4.73077 14.4322 1 10.1553 1C5.87837 1 2.30145 4.73077 1.40145 5.74615C1.34269 5.8177 1.31058 5.90742 1.31058 6C1.31058 6.09258 1.34269 6.1823 1.40145 6.25385C2.30145 7.26923 5.87837 11 10.1553 11C14.4322 11 18.0091 7.26923 18.9091 6.25385C18.9679 6.1823 19 6.09258 19 6C19 5.90742 18.9679 5.8177 18.9091 5.74615Z"
+      stroke="#7F7F7F"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+);
+const closeEyeIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="20"
+    height="12"
+    viewBox="0 0 20 12"
+    fill="none"
+  >
+    <path
+      d="M7.46298 6.00005C7.46298 6.71409 7.74664 7.39889 8.25154 7.9038C8.75645 8.4087 9.44125 8.69236 10.1553 8.69236C10.8693 8.69236 11.5541 8.4087 12.059 7.9038C12.5639 7.39889 12.8476 6.71409 12.8476 6.00005C12.8476 5.286 12.5639 4.6012 12.059 4.0963C11.5541 3.59139 10.8693 3.30774 10.1553 3.30774C9.44125 3.30774 8.75645 3.59139 8.25154 4.0963C7.74664 4.6012 7.46298 5.286 7.46298 6.00005Z"
+      stroke="#7F7F7F"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M18.9091 5.74615C18.0091 4.73077 14.4322 1 10.1553 1C5.87837 1 2.30145 4.73077 1.40145 5.74615C1.34269 5.8177 1.31058 5.90742 1.31058 6C1.31058 6.09258 1.34269 6.1823 1.40145 6.25385C2.30145 7.26923 5.87837 11 10.1553 11C14.4322 11 18.0091 7.26923 18.9091 6.25385C18.9679 6.1823 19 6.09258 19 6C19 5.90742 18.9679 5.8177 18.9091 5.74615Z"
+      stroke="#7F7F7F"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <line
+      x1="14.0303"
+      y1="2.03033"
+      x2="6.03033"
+      y2="10.0303"
+      stroke="#7F7F7F"
+      stroke-width="1.5"
+    />
+  </svg>
+);
 const formSchema = z.object({
   password: z.string().min(2).max(50),
   confirmPassword: z.string().min(2).max(50),
@@ -78,94 +133,97 @@ export const PasswordReset = (props: TFunctionProps) => {
     },
   });
   const { toast } = useToast();
-
+  const [enableSubmitButton, setEnableSubmitButton] = useState(true);
   // Note this is the claim challenge check - if successfull comes back the password reset UI should be accasible - if not we have to close the menu again
   useEffect(() => {
     if (props.comingFromRedirect) {
       resetPassword();
+      togglePasswordResetUI();
     }
   }, []);
 
   const resetPassword = () => {
     const passwordToSet = passwordDisplay.value;
     if (!getIsPasswordValid(passwordToSet)) {
-      return;
-    }
-    setPasswordDisplay({
-      visible: false,
-      value: "",
-      showValue: false,
-      valueConfirm: "",
-      showValueConfirm: false,
-      loading: true,
-    });
-
-    // Call the API to reset the password
-    callResetPassword(passwordToSet)
-      .then(() => {
-        setPasswordDisplay({
-          visible: false,
-          value: "",
-          showValue: false,
-          valueConfirm: "",
-          showValueConfirm: false,
-          loading: false,
-        });
-      })
-      .catch((error) => {
-        setPasswordDisplay({
-          visible: true,
-          value: "",
-          showValue: false,
-          valueConfirm: "",
-          showValueConfirm: false,
-          loading: false,
-          errorValue: "ERROR",
-          errorValueConfirm: "ERROR",
-        });
-        toast({
-          variant: "destructive",
-          title: "Something went wrong during Password reset generation.",
-          description: error.response.statusText,
-        });
+      toast({
+        variant: "destructive",
+        title: "Something went wrong during password reset",
+        description: PASSWORD_MUST_CONTAIN_ERROR_TEXT,
       });
+    } else if (passwordToSet !== passwordDisplay.valueConfirm) {
+      toast({
+        variant: "destructive",
+        title: "Something went wrong during password reset",
+        description: PASSWORD_MUST_BE_SAME_ERROR_TEXT,
+      });
+    } else {
+      // Call the API to reset the password
+      callResetPassword(passwordToSet)
+        .then(() => {
+          setPasswordDisplay({
+            visible: false,
+            value: "",
+            showValue: false,
+            valueConfirm: "",
+            showValueConfirm: false,
+            loading: false,
+          });
+        })
+        .catch((error) => {
+          setPasswordDisplay({
+            visible: true,
+            value: "",
+            showValue: false,
+            valueConfirm: "",
+            showValueConfirm: false,
+            loading: false,
+            errorValue: "ERROR",
+            errorValueConfirm: "ERROR",
+          });
+          toast({
+            variant: "destructive",
+            title: "Something went wrong during Password reset generation.",
+            description: error.response.statusText,
+          });
+        });
+    }
   };
 
   const togglePasswordResetUI = () => {
-    // if (!passwordDisplay.visible) {
-    //   checkResetPasswordClaim();
-    // }
-    // setPasswordDisplay({
-    //   visible: !passwordDisplay.visible,
-    //   value: "",
-    //   showValue: false,
-    //   valueConfirm: "",
-    //   showValueConfirm: false,
-    //   loading: false,
-    // });
-    setPasswordDisplay({
-      visible: true,
-      value: "",
-      showValue: false,
-      valueConfirm: "",
-      showValueConfirm: false,
-      loading: false,
-    });
+    if (!passwordDisplay.visible) {
+      setPasswordDisplay({
+        visible: true,
+        value: "",
+        showValue: false,
+        valueConfirm: "",
+        showValueConfirm: false,
+        loading: false,
+      });
+    } else {
+      setPasswordDisplay({
+        visible: false,
+        value: "",
+        showValue: false,
+        valueConfirm: "",
+        showValueConfirm: false,
+        loading: false,
+      });
+    }
   };
 
-  // const handleClickShowPassword = (inputType: PasswordInputType) => {
-  //   setPasswordDisplay((oldValues) => {
-  //     let propertyToSet = "showValue";
-  //     let valueToSet = !oldValues.showValue;
+  const handleClickShowPassword = (inputType: PasswordInputType) => {
+    setPasswordDisplay((oldValues) => {
+      let propertyToSet = "showValue";
+      let valueToSet = !oldValues.showValue;
 
-  //     if (inputType == PasswordInputType.CONFIRM) {
-  //       propertyToSet = "showValueConfirm";
-  //       valueToSet = !oldValues.showValueConfirm;
-  //     }
+      if (inputType == PasswordInputType.CONFIRM) {
+        propertyToSet = "showValueConfirm";
+        valueToSet = !oldValues.showValueConfirm;
+      }
 
-  //     return { ...oldValues, [propertyToSet]: valueToSet };
-  //   });
-  // };
+      return { ...oldValues, [propertyToSet]: valueToSet };
+    });
+  };
 
   const getIsPasswordValid = (password: string) => {
     if (password.length < 8 || password.length > 255) {
@@ -194,7 +252,11 @@ export const PasswordReset = (props: TFunctionProps) => {
   const handleInput = (inputType: PasswordInputType, value: string) => {
     setPasswordDisplay((oldValues) => {
       let propertyToSet = "value";
-
+      if (value.length > 0) {
+        setEnableSubmitButton(false);
+      } else {
+        setEnableSubmitButton(true);
+      }
       if (inputType == PasswordInputType.CONFIRM) {
         propertyToSet = "valueConfirm";
       }
@@ -218,15 +280,9 @@ export const PasswordReset = (props: TFunctionProps) => {
       return { ...newValues, errorValueConfirm, errorValue };
     });
   };
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    handleInput(PasswordInputType.CONFIRM, values.password);
+  function onSubmit() {
     resetPassword();
   }
-  // const handleMouseDownPassword = (
-  //   event: React.MouseEvent<HTMLButtonElement>
-  // ) => {
-  //   event.preventDefault();
-  // };
 
   return (
     <div>
@@ -248,19 +304,33 @@ export const PasswordReset = (props: TFunctionProps) => {
       {passwordDisplay.visible && (
         <>
           <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2">
+            <form className="space-y-2">
               <FormField
                 control={form.control}
                 name="password"
                 render={({ field }) => (
                   <FormItem>
                     <FormControl>
-                      <Input
-                        type="password"
-                        className="action-card__pasword-reset__input"
-                        placeholder="Enter Password"
-                        {...field}
-                      />
+                      <div className="action-card__pasword-reser__input-container">
+                        <Input
+                          type={passwordDisplay.showValue ? "text" : "password"}
+                          className="action-card__pasword-reset__input"
+                          placeholder="Enter Password"
+                          {...field}
+                          onChange={(e) =>
+                            handleInput(PasswordInputType.MAIN, e.target.value)
+                          }
+                          value={passwordDisplay.value}
+                        />
+                        <div
+                          className="action-card__pasword-reser__input-container__icon"
+                          onClick={() =>
+                            handleClickShowPassword(PasswordInputType.MAIN)
+                          }
+                        >
+                          {passwordDisplay.showValue ? closeEyeIcon : eyeIcon}
+                        </div>
+                      </div>
                     </FormControl>
                   </FormItem>
                 )}
@@ -271,12 +341,29 @@ export const PasswordReset = (props: TFunctionProps) => {
                 render={({ field }) => (
                   <FormItem>
                     <FormControl>
-                      <Input
-                        className="action-card__pasword-reset__input"
-                        type="password"
-                        placeholder="Confirm Password"
-                        {...field}
-                      />
+                      <div className="action-card__pasword-reser__input-container">
+                        <Input
+                          className="action-card__pasword-reset__input"
+                          type={passwordDisplay.showValue ? "text" : "password"}
+                          placeholder="Confirm Password"
+                          {...field}
+                          onChange={(e) =>
+                            handleInput(
+                              PasswordInputType.CONFIRM,
+                              e.target.value
+                            )
+                          }
+                          value={passwordDisplay.valueConfirm}
+                        />
+                        <div
+                          className="action-card__pasword-reser__input-container__icon"
+                          onClick={() =>
+                            handleClickShowPassword(PasswordInputType.MAIN)
+                          }
+                        >
+                          {passwordDisplay.showValue ? closeEyeIcon : eyeIcon}
+                        </div>
+                      </div>
                     </FormControl>
                   </FormItem>
                 )}
@@ -284,6 +371,10 @@ export const PasswordReset = (props: TFunctionProps) => {
               <Button
                 className="action-card__pasword-reset__submit-button"
                 type="submit"
+                disabled={enableSubmitButton}
+                onClick={() => {
+                  onSubmit();
+                }}
               >
                 Submit
               </Button>
@@ -291,127 +382,6 @@ export const PasswordReset = (props: TFunctionProps) => {
           </Form>
         </>
       )}
-      {/* {passwordDisplay.visible && (
-        <div>
-          <FormControl
-            sx={{ mt: 2, width: "100%" }}
-            variant="outlined"
-            error={passwordDisplay.errorValue ? true : false}
-          >
-            <InputLabel htmlFor="outlined-adornment-password">
-              Password
-            </InputLabel>
-            <OutlinedInput
-              id="outlined-adornment-password"
-              type={passwordDisplay.showValue ? "text" : "password"}
-              onChange={(e) =>
-                handleInput(PasswordInputType.MAIN, e.target.value)
-              }
-              aria-describedby="component-error-text"
-              value={passwordDisplay.value}
-              endAdornment={
-                <InputAdornment position="end">
-                  <IconButton
-                    aria-label="toggle password visibility"
-                    onClick={() =>
-                      handleClickShowPassword(PasswordInputType.MAIN)
-                    }
-                    onMouseDown={handleMouseDownPassword}
-                    edge="end"
-                  >
-                    {passwordDisplay.showValue ? (
-                      <VisibilityOff />
-                    ) : (
-                      <Visibility />
-                    )}
-                  </IconButton>
-                </InputAdornment>
-              }
-              label="Password"
-            />
-            <FormHelperText id="component-error-text">
-              {passwordDisplay.errorValue}
-            </FormHelperText>
-          </FormControl>
-          <FormControl
-            sx={{ mt: 1, width: "100%" }}
-            variant="outlined"
-            error={passwordDisplay.errorValueConfirm ? true : false}
-          >
-            <InputLabel htmlFor="outlined-adornment-password">
-              Password
-            </InputLabel>
-            <OutlinedInput
-              id="outlined-adornment-password"
-              type={passwordDisplay.showValueConfirm ? "text" : "password"}
-              onChange={(e) =>
-                handleInput(PasswordInputType.CONFIRM, e.target.value)
-              }
-              aria-describedby="component-error-text"
-              value={passwordDisplay.valueConfirm}
-              endAdornment={
-                <InputAdornment position="end">
-                  <IconButton
-                    aria-label="toggle password visibility"
-                    onClick={() =>
-                      handleClickShowPassword(PasswordInputType.CONFIRM)
-                    }
-                    onMouseDown={handleMouseDownPassword}
-                    edge="end"
-                  >
-                    {passwordDisplay.showValueConfirm ? (
-                      <VisibilityOff />
-                    ) : (
-                      <Visibility />
-                    )}
-                  </IconButton>
-                </InputAdornment>
-              }
-              label="Confirm Password"
-            />
-            <FormHelperText id="component-error-text">
-              {passwordDisplay.errorValueConfirm}
-            </FormHelperText>
-          </FormControl>
-          <Box textAlign="center">
-            <Button
-              sx={{ mt: 1 }}
-              variant="contained"
-              onClick={resetPassword}
-              disabled={
-                passwordDisplay.loading ||
-                passwordDisplay.errorValue ||
-                passwordDisplay.errorValueConfirm ||
-                passwordDisplay.value === "" ||
-                passwordDisplay.valueConfirm === ""
-                  ? true
-                  : false
-              }
-            >
-              Apply
-            </Button>
-          </Box>
-        </div>
-      )}
-      <div
-        className={
-          passwordDisplay.loading
-            ? "function_plane__function_component__loading_spinner__container"
-            : "hidden_element"
-        }
-      >
-        <CircularProgress />
-      </div> }
-      { {passwordDisplay.visible && ()}
-      <div
-        className={
-          passwordDisplay.loading
-            ? "function_plane__function_component__loading_spinner__container"
-            : "hidden_element"
-        }
-      >
-        <CircularProgress />
-      </div> */}
     </div>
   );
 };
