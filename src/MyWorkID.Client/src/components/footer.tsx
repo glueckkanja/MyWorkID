47720f32049a12e3ec7e33c5511852fe640c9fb1
Stefan Heller <stefan.heller@glueckkanja-gab.com>
2025-01-23 14:42:55 +0100

    change icons to svg and adjust css.

--- src/MyWorkID.Client/src/components/footer.tsx
+++ src/MyWorkID.Client/src/components/footer.tsx
@@ -1,11 +1,21 @@
 import BrandingSvg from "../assets/svg/branding.svg";
+import BrandingSvgDark from "../assets/svg/branding-dark.svg";
 
 export const Footer = () => {
   return (
     <div className="footer__container">
       <span className="footer__text">powered by</span>
       <div className="footer_svg-icon">
-        <img src={BrandingSvg} alt="BrandingLogo" />
+        <img
+          className="footer_svg-icon-light"
+          src={BrandingSvg}
+          alt="BrandingLogo"
+        />
+        <img
+          className="footer_svg-icon-dark"
+          src={BrandingSvgDark}
+          alt="BrandingLogo"
+        />
       </div>
     </div>
   );
