383bc2aa50d107719759da661cf9cfd4af691e48
Stefan Heller <88711548+StefanH114@users.noreply.github.com>
2025-01-23 16:30:25 +0100

    export svg from assets folder (#39)
    * export svg from assets folder
    
    * make asset folder for design svg
    
    * change icons to svg and adjust css.
    
    * cleanup css
    
    * remove semicolon

--- src/MyWorkID.Client/src/components/function-plane/function-plane-components/create-tap.tsx
+++ src/MyWorkID.Client/src/components/function-plane/function-plane-components/create-tap.tsx
@@ -9,65 +9,12 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { useToast } from "@/hooks/use-toast";
-
+import  CreateTapSvgIcon  from "@/assets/svg/create-tap.svg";
 type TAPDisplay = {
   visible: boolean;
   value: string;
   loading: boolean;
 };
-
-const svgIcon = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="52"
-    height="44"
-    viewBox="0 0 52 44"
-    fill="none"
-  >
-    <path
-      d="M37.375 42.0416C47.7838 42.0411 54.2896 30.7562 49.0847 21.7286C46.6692 17.539 42.2055 14.9583 37.375 14.9583C26.9661 14.9583 20.4606 26.243 25.665 35.2707C28.0805 39.4608 32.544 42.0418 37.375 42.0416Z"
-      stroke="white"
-      strokeWidth="2"
-      strokeLinecap="square"
-    />
-    <path
-      d="M43.3333 24.7083L37.0801 32.7524C36.4895 33.5094 35.342 33.5887 34.6455 32.9206L31.4167 29.8051"
-      stroke="white"
-      strokeWidth="2"
-      strokeLinecap="square"
-    />
-    <path
-      d="M14.6175 20.3749H5.32246C2.9812 20.3749 1.08333 18.5426 1.08333 16.2823V6.05084C1.08333 3.79052 2.9812 1.95825 5.32246 1.95825H45.5942C47.9355 1.95825 49.8333 3.79052 49.8333 6.05084V7.86253"
-      stroke="white"
-      strokeWidth="2"
-      strokeLinecap="square"
-    />
-    <path
-      d="M21.2343 11.2802C20.8359 11.2802 20.5868 10.8489 20.786 10.5039C20.8785 10.3438 21.0494 10.2451 21.2343 10.2451"
-      stroke="white"
-      strokeWidth="2"
-      strokeLinecap="round"
-    />
-    <path
-      d="M21.2346 11.2802C21.6331 11.2802 21.8821 10.8489 21.6829 10.5039C21.5904 10.3438 21.4195 10.2451 21.2346 10.2451"
-      stroke="white"
-      strokeWidth="2"
-      strokeLinecap="round"
-    />
-    <path
-      d="M11.3939 11.2802C10.9955 11.2802 10.7464 10.8489 10.9456 10.5039C11.0382 10.3438 11.2091 10.2451 11.3939 10.2451"
-      stroke="white"
-      strokeWidth="2"
-      strokeLinecap="round"
-    />
-    <path
-      d="M11.3942 11.2802C11.7926 11.2802 12.0417 10.8489 11.8425 10.5039C11.75 10.3438 11.5791 10.2451 11.3942 10.2451"
-      stroke="white"
-      strokeWidth="2"
-      strokeLinecap="round"
-    />
-  </svg>
-);
 
 export const CreateTAP = (props: TFunctionProps) => {
   const [tapDisplay, setTapDisplay] = useState<TAPDisplay>({
@@ -125,7 +72,7 @@ export const CreateTAP = (props: TFunctionProps) => {
           }}
         >
           <CardHeader>
-            <CardTitle>{svgIcon}</CardTitle>
+            <CardTitle><img src={CreateTapSvgIcon} alt="CreateTapIcon" /></CardTitle>
           </CardHeader>
           <CardFooter className="action-card__footer">
             Create Temporary Access Password
--- src/MyWorkID.Client/src/components/function-plane/function-plane-components/dismiss-userisk.tsx
+++ src/MyWorkID.Client/src/components/function-plane/function-plane-components/dismiss-userisk.tsx
@@ -5,76 +5,12 @@ import { Card, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import CircularProgress from "@mui/material/CircularProgress/CircularProgress";
 import { CardContent } from "@mui/material";
 import { useToast } from "@/hooks/use-toast";
+import DismissUserRiskSvg from "@/assets/svg/dismiss-user-risk.svg";
+import SuccessSvg from "../../../assets/svg/success.svg";
 
 export const DismissUserRisk = (props: TFunctionProps) => {
   const [loading, setLoading] = useState<boolean>(false);
   const [success, setSuccess] = useState<boolean>();
-  const svgIcon = (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="36"
-      height="46"
-      viewBox="0 0 36 46"
-      fill="none"
-    >
-      <path
-        d="M16.1158 9.14827V5.10132H12.3477V9.14827C10.9875 9.55381 9.79442 10.3874 8.94583 11.5252C8.09724 12.663 7.63843 14.0442 7.63757 15.4636V42.7824C7.63757 43.2821 7.83607 43.7613 8.1894 44.1146C8.54273 44.468 9.02195 44.6665 9.52163 44.6665H18.9419C19.4416 44.6665 19.9208 44.468 20.2741 44.1146C20.6275 43.7613 20.826 43.2821 20.826 42.7824V15.4636C20.8251 14.0442 20.3663 12.663 19.5177 11.5252C18.6691 10.3874 17.476 9.55381 16.1158 9.14827Z"
-        stroke="white"
-        strokeWidth="2"
-        strokeLinecap="square"
-      />
-      <path
-        d="M3.86953 1.33325L12.3478 5.10136C9.59952 5.10136 6.96383 6.1931 5.02052 8.13641C3.07721 10.0797 1.98547 12.7154 1.98547 15.4637V39.0144"
-        stroke="white"
-        strokeWidth="2"
-        strokeLinecap="square"
-      />
-      <path
-        d="M32.9481 1.34272C33.081 1.32501 33.2162 1.3359 33.3446 1.37465C33.473 1.41339 33.5916 1.47911 33.6926 1.5674C33.7935 1.65569 33.8745 1.76451 33.93 1.8866C33.9855 2.00868 34.0143 2.14121 34.0144 2.27532V7.92749C34.0156 8.06239 33.9878 8.19598 33.9329 8.3192C33.878 8.44243 33.7972 8.55242 33.6961 8.64174C33.595 8.73106 33.4759 8.79762 33.3469 8.83691C33.2178 8.87621 33.0818 8.88733 32.9481 8.86951L21.5194 7.34531C21.0669 7.28505 20.6516 7.06253 20.3508 6.71911C20.05 6.37569 19.8841 5.93474 19.884 5.47822V4.73401C19.8837 4.27717 20.0494 3.83576 20.3502 3.49194C20.651 3.14813 21.0665 2.92534 21.5194 2.86503L32.9481 1.34272Z"
-        stroke="white"
-        strokeWidth="2"
-        strokeLinecap="square"
-      />
-      <path
-        d="M16.1158 5.10132H19.884"
-        stroke="white"
-        strokeWidth="2"
-        strokeLinecap="square"
-      />
-      <path
-        d="M20.826 39.0145H16.1159C15.866 39.0145 15.6264 38.9153 15.4497 38.7386C15.2731 38.5619 15.1738 38.3223 15.1738 38.0725V19.2319C15.1738 18.9821 15.2731 18.7425 15.4497 18.5658C15.6264 18.3892 15.866 18.2899 16.1159 18.2899H20.826"
-        stroke="white"
-        strokeWidth="2"
-        strokeLinecap="square"
-      />
-    </svg>
-  );
-  const svgIconSuccess = (
-    <svg
-      width="42"
-      height="42"
-      viewBox="0 0 42 42"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <g id="Group 33">
-        <path
-          id="Vector"
-          d="M21.0002 41.0003C36.3734 40.9996 45.982 24.3325 38.2947 10.9994C34.7272 4.81159 28.1345 1 21.0002 1C5.62697 1 -3.98128 17.6668 3.70532 31.0002C7.27283 37.1887 13.8651 41.0007 21.0002 41.0003Z"
-          stroke="#ACD653"
-          strokeWidth="2"
-          strokeLinecap="square"
-        />
-        <path
-          id="Vector_2"
-          d="M29.8001 15.4001L20.5645 27.2808C19.6922 28.3989 17.9975 28.5159 16.9688 27.5293L12.2 22.9279"
-          stroke="#ACD653"
-          strokeWidth="2"
-          strokeLinecap="square"
-        />
-      </g>
-    </svg>
-  );
   const { toastException } = useToast();
 
   useEffect(() => {
@@ -107,7 +43,7 @@ export const DismissUserRisk = (props: TFunctionProps) => {
           }}
         >
           <CardHeader>
-            <CardTitle>{svgIcon}</CardTitle>
+            <CardTitle><img src={DismissUserRiskSvg} alt="DismissUserRiskIcon" /></CardTitle>
           </CardHeader>
           <CardFooter className="action-card__footer">
             Dismiss User Risk
@@ -124,7 +60,9 @@ export const DismissUserRisk = (props: TFunctionProps) => {
       {!loading && success === true && (
         <Card className="action-card__validate-id-success">
           <CardHeader>
-            <CardTitle>{svgIconSuccess}</CardTitle>
+            <CardTitle>
+              <img src={SuccessSvg} alt="SuccessIcon" />
+            </CardTitle>
           </CardHeader>
           <CardFooter className="action-card__footer__validate-id-success">
             User Risk Dismissed.
