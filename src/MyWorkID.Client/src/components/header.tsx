09502a111b917818d06f00e4a42cd915892163b8
Stefan Heller <stefan.heller@glueckkanja-gab.com>
2025-03-31 11:24:48 +0200

    add logout button

--- src/MyWorkID.Client/src/components/header.tsx
+++ src/MyWorkID.Client/src/components/header.tsx
@@ -1,6 +1,9 @@
 import { useTheme } from "./use-theme";
 import HeaderLogoSvg from "../assets/svg/header-logo.svg";
 import DarkModeSvg from "../assets/svg/dark-mode-toggle.svg";
+import LogOutSvg from "../assets/svg/log-out.svg";
+import { useMsal } from "@azure/msal-react";
+import { useToast } from "@/hooks/use-toast";
 
 enum ColorTheme {
   Light = "light",
@@ -9,7 +12,9 @@ enum ColorTheme {
 const STORAGEKEY = "vite-ui-theme";
 type Theme = ColorTheme.Dark | ColorTheme.Light;
 export const Header = () => {
+  const { instance } = useMsal();
   const { setTheme } = useTheme();
+  const { toastError } = useToast();
   const setColorTheme = () => {
     if ((localStorage.getItem(STORAGEKEY) as Theme) === "dark") {
       setTheme(ColorTheme.Light);
@@ -17,7 +22,11 @@ export const Header = () => {
       setTheme(ColorTheme.Dark);
     }
   };
-
+  const handleLogoutRedirect = () => {
+    instance
+      .logoutRedirect()
+      .catch(() => toastError("Logout failed. Please try again."));
+  };
   return (
     <div className="header__container">
       <img src={HeaderLogoSvg} alt="myWorkID Logo" />
@@ -25,18 +34,26 @@ export const Header = () => {
         <span className="header__title__text-myWork">MyWork</span>
         <span className="header__title__text-ID">ID</span>
       </div>
-      <button
-        className="header__dark-mode-toggle"
-        tabIndex={0}
-        onMouseDown={() => setColorTheme()}
-        onKeyDown={(e) => {
-          if (e.key === "Enter" || e.key === " ") {
-            setColorTheme();
-          }
-        }}
-      >
-        <img src={DarkModeSvg} alt="DarkModeToggle" />
-      </button>
+      <div className="header__buttons">
+        <button
+          className="header__buttons__logout-button"
+          onClick={handleLogoutRedirect}
+        >
+          <img src={LogOutSvg} alt="LogOutButton" />
+        </button>
+        <button
+          className="header__buttons__dark-mode-toggle"
+          tabIndex={0}
+          onMouseDown={() => setColorTheme()}
+          onKeyDown={(e) => {
+            if (e.key === "Enter" || e.key === " ") {
+              setColorTheme();
+            }
+          }}
+        >
+          <img src={DarkModeSvg} alt="DarkModeToggle" />
+        </button>
+      </div>
     </div>
   );
 };
