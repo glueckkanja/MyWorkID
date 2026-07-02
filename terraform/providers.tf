a1a71d4d07123f7d423c77f43574385f938f4a1f
Philipp Muhr <45574344+Philivanei@users.noreply.github.com>
2026-07-02 09:54:36 +0200

    fix: apply dotnet 10 upgrade to terraform (#151)
    * feat: upgrade terraform scripts to dotnet 10
    
    * fix: add new lock file

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,15 +2,15 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.19.0"
+      version = "4.79.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "3.1.0"
+      version = "3.9.0"
     }
     time = {
       source  = "hashicorp/time"
-      version = "0.12.1"
+      version = "0.14.0"
     }
   }
 }
