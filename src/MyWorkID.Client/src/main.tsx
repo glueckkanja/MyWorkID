95fdedf83a36f24731d415d62cf5046c9756f473
Philipp Muhr <45574344+Philivanei@users.noreply.github.com>
2026-09-16 11:01:46 +0200

    chore: address sonarqube issue

--- src/MyWorkID.Client/src/main.tsx
+++ src/MyWorkID.Client/src/main.tsx
@@ -12,33 +12,31 @@ import {
 } from "./services/frontend-options-service";
 import { getMaximumDismissibleRiskLevel, RiskLevel } from "./lib/risk-level";
 
-void (async () => {
-  let maxDismissibleRiskLevel: RiskLevel = RiskLevel.Low;
-  try {
-    const frontendOptions = await getFrontendOptions();
-    updateDocumentHead(frontendOptions);
-    maxDismissibleRiskLevel = getMaximumDismissibleRiskLevel(
-      frontendOptions.maxDismissibleRiskLevel,
-    );
-  } catch (error) {
-    console.debug(
-      "Failed to load frontend options; falling back to maxDismissibleRiskLevel Low",
-      error,
-    );
-  }
-
-  const root = ReactDOM.createRoot(
-    document.getElementById("root") as HTMLElement,
+let maxDismissibleRiskLevel: RiskLevel = RiskLevel.Low;
+try {
+  const frontendOptions = await getFrontendOptions();
+  updateDocumentHead(frontendOptions);
+  maxDismissibleRiskLevel = getMaximumDismissibleRiskLevel(
+    frontendOptions.maxDismissibleRiskLevel,
   );
-
-  root.render(
-    <BrowserRouter>
-      <React.StrictMode>
-        <AppAuthentication>
-          <App maxDismissibleRiskLevel={maxDismissibleRiskLevel} />
-          <Toaster />
-        </AppAuthentication>
-      </React.StrictMode>
-    </BrowserRouter>,
+} catch (error) {
+  console.debug(
+    "Failed to load frontend options; falling back to maxDismissibleRiskLevel Low",
+    error,
   );
-})();
+}
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement,
+);
+
+root.render(
+  <BrowserRouter>
+    <React.StrictMode>
+      <AppAuthentication>
+        <App maxDismissibleRiskLevel={maxDismissibleRiskLevel} />
+        <Toaster />
+      </AppAuthentication>
+    </React.StrictMode>
+  </BrowserRouter>,
+);
